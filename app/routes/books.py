58d0d88044d5f1c6ad9cbaeb7e72c2fda90a9925
marjunstudio <uebujie@gmail.com>
2024-06-15 15:27:51 +0900

    feat:GoogleブックスAPIを叩いて書籍情報を取得

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,37 +1,20 @@
-from fastapi import Depends, HTTPException, APIRouter
-from sqlalchemy.orm import Session
-from models.book import Book
-from schemas.book import BookBase
-
-from app.dependencies import get_db
+from fastapi import APIRouter
+from pydantic import BaseModel
+import requests
 
 router = APIRouter()
 
-#TODO:おそらく使用しないエンドポイントなので削除予定
-# def get_book_by_id(book_id: str, db: Session = Depends(get_db)):
-#     book_data = db.query(Book).filter(Book.id == book_id).one_or_none()
-#     return book_data
+
+class BookRequest(BaseModel):
+    title: str
 
 
-# @router.post("/books")
-# def create_book(book: BookBase, db: Session = Depends(get_db)):
-#     book_data = get_book_by_id(book.id, db)
-#     if book_data:
-#         raise HTTPException(status_code=400, detail="Book already registered")
+@router.post("/v1/books", tags=["books"], summary="GoogleBooksAPIを使用して、書籍情報を取得する")
+def get_books_info(book: BookRequest):
+    url = (f"https://www.googleapis.com/books/v1/volumes?q={book.title}&key=AIzaSyCixJ4hHo0Mz6Wx8Z1DRFVnHjM8M0dTJN0&maxResults=10")
+    print(url)
+    results = requests.get(url)
 
-#     try:
-#         print("保存処理を開始")
-#         db_book = Book(
-#             id=book.id,
-#             title=book.title,
-#             description=book.description,
-#             published_date=book.published_date,
-#             page_count=book.page_count,
-#             thumbnail=book.thumbnail,
-#         )
-#         db.add(db_book)
-#         db.commit()
-#         db.refresh(db_book)
-#         return db_book
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"Unexpected error occurred: {str(e)}")
+    print(results.json())
+
+    return results.json()
